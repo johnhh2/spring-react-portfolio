3f4c02c345eb35aa01e282737baa816c72046574
Matt Purnell <mattpurnell98@gmail.com>
2022-09-02 17:01:51 -0500

    Add simple POST method

--- spring-portfolio/src/main/java/com/portfolio/ApiController.java
+++ spring-portfolio/src/main/java/com/portfolio/ApiController.java
@@ -3,6 +3,7 @@ package com.example.springboot;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -19,15 +20,26 @@ public class ApiController {
             this.key = val;
         }
 
+        public int increment_by(int val) {
+            this.key += val;
+            return this.key;
+        }
+
         public String toString() {
             return "{\"key\": " + this.key + ", \"foo\": \"bar\"}";
         }
     }
 
+    public JSONResponse response = new JSONResponse(0);
+
 	@RequestMapping(value="/api", method=RequestMethod.GET,
                     produces=MediaType.APPLICATION_JSON_VALUE)
-	public String index() {
-		return new JSONResponse(0xdeadbeef).toString();
+	public String json() {
+		return this.response.toString();
 	}
+    @RequestMapping(value="/api/edit", method=RequestMethod.POST)
+    public int edit(@RequestBody int val) {
+        return this.response.increment_by(val);
+    }
 
 }
