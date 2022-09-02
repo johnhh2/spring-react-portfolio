160636ff1edc27b8999e12da7363e39de20e6605
johnhh2 <31453234+johnhh2@users.noreply.github.com>
2022-09-02 16:17:07 -0500

    Added test page

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      key: 0,
+      foo: "foo",
+    };
+  }
+
+  componentDidMount() {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      data: {},
+    };
+    fetch('http://localhost:8080/api/', requestOptions)
+      .then(async response => {
+         const data = await response.json();
+         this.setState({
+           key: data.key,
+           foo: data.foo,
+         });
+         this.render();
+      })
+      .catch(error => {
+        this.setState({ errorMessage: error.toString() });
+        console.error('Error', error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Here is your data: {this.state.key} {this.state.foo}
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
