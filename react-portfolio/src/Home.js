f927b0aedf33b047b35919985d8434760fe6fb35
johnhh2 <31453234+johnhh2@users.noreply.github.com>
2022-09-05 16:08:42 -0500

    Added authors and frameworks to homepage

--- react-portfolio/src/Home.js
+++ react-portfolio/src/Home.js
@@ -10,10 +10,20 @@ const serverAddress = config.SERVER_ADDR;
 class Home extends React.Component {
 
   render() {
+    let authors = config.AUTHORS;
+    if (Math.random() < 0.5) {
+      authors = authors.reverse();
+    }
+    let lastAuthor = authors.pop();
+    let authorText = authors.join(', ') + ' and ' + lastAuthor;
+    
     return (
       <div className="Home">
         <header className="Home-header">
 	  <h1>Portfolio Project</h1>
+          <div className="Home-authors">
+            by {authorText}
+          </div>
           <div className="Home-framework">
 	    Created with
             <img src={logo} className="React-logo" alt="logo" />
@@ -21,6 +31,7 @@ class Home extends React.Component {
             <img src={springLogo} className="Spring-logo" alt="logo" />
             Spring
           </div>
+            
         </header>
       </div>
     );
