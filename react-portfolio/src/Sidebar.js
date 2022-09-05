c3f485b99a766cdb15f34648440fa3fcc3d9e21c
johnhh2 <31453234+johnhh2@users.noreply.github.com>
2022-09-05 14:12:55 -0500

    Hid sidebar for small screens

--- react-portfolio/src/Sidebar.js
+++ react-portfolio/src/Sidebar.js
@@ -6,13 +6,37 @@ import config from './config.json';
 const serverAddress = config.SERVER_ADDR;
 
 class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {hideNav: false};
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.resize.bind(this));
+    this.resize();
+  }
+
+  resize() {
+    let currentHideNav = (window.innerWidth < 760);
+    if (currentHideNav !== this.state.hideNav) {
+      this.setState({hideNav: currentHideNav});
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.resize.bind(this));
+  }
 
   render() {
-    return (
-      <div className="Sidebar">
-        <a className="Sidebar-tab">Home</a>
-      </div>
-    );
+    if (this.state.hideNav != true) {
+       return (
+         <div className="Sidebar">
+           <a className="Sidebar-tab">Home</a>
+         </div>
+       );
+    } else {
+      return null;
+    }
   }
 }
 export default Sidebar;
