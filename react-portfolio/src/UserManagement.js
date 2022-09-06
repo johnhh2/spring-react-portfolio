9862eefe4075aca7b429f007dd6eaf94eaff4ddc
Matt Purnell <mattpurnell98@gmail.com>
2022-09-05 20:24:04 -0500

    React: Improve user management form

--- react-portfolio/src/UserManagement.js
+++ react-portfolio/src/UserManagement.js
@@ -8,16 +8,41 @@ class UserManagement extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+        key: 0,
+        username: "",
+        email: "",
+        age: 0
     };
   }
 
-  createTestUser() {
+  handleChange(event) {
+    switch (event.target.id) {
+      case "username":
+        this.setState({username: event.target.value});
+        break;
+      case "email":
+        this.setState({email: event.target.value});
+        break;
+      case "age":
+        this.setState({age: event.target.value});
+        break;
+      default:
+        console.error("Unexpected event target: ", event.target);
+        break;
+    }
+  }
+
+  handleSubmit(event) {
     const requestOptions = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ key: 0, username: "testuser", email: "test@user.com", dark_mode: true, age: 25})
+      body: JSON.stringify({
+        key: this.state.key,
+        username: this.state.username,
+        email: this.state.email,
+        age: this.state.age})
     };
     fetch(`${serverAddress}/api/create_user`, requestOptions)
       .then(async response => {
@@ -25,6 +50,7 @@ class UserManagement extends React.Component {
         console.log(data)
         if (data.success) {
           console.log("user created: " + data.user.toString());
+          this.setState({key: this.state.key + 1});
           this.render();
         }
         else {
@@ -34,13 +60,22 @@ class UserManagement extends React.Component {
       .catch(error => {
         console.error('Error', error);
       });
+    event.preventDefault();
   }
 
   render() {
     return (
       <div className="UserManagement">
         <header className="UserManagement-header">
-          <button onClick={this.createTestUser.bind(this)}>Create Test User</button>
+          <form onSubmit={this.handleSubmit.bind(this)}>
+          <label htmlFor="username">Username: </label>
+          <input type="text" id="username" name="username" value={this.state.username} onChange={this.handleChange.bind(this)} /><br/>
+          <label htmlFor="email">Email: </label>
+          <input type="email" id="email" name="email" value={this.state.email} onChange={this.handleChange.bind(this)} /><br />
+          <label htmlFor="age">Age: </label>
+          <input type="number" id="age" name="age" value={this.state.age} onChange={this.handleChange.bind(this)} /><br />
+          <input type="submit" id="submit" name="Create User" />
+          </form>
         </header>
       </div>
     );
