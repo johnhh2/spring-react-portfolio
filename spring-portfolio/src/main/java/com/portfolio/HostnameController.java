a59feeb0de779ba68516686d92308a6dded3f9b1
Matt Purnell <mattpurnell98@gmail.com>
2022-09-13 17:49:58 -0500

    Spring: Clean up imports in HostnameController

--- spring-portfolio/src/main/java/com/portfolio/HostnameController.java
+++ spring-portfolio/src/main/java/com/portfolio/HostnameController.java
@@ -1,22 +1,17 @@
 package com.portfolio;
 
 import java.util.List;
-
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import com.portfolio.User;
 
 @CrossOrigin(origins="http://localhost:3000")
 @RestController
